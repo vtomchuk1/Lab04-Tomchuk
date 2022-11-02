6bbd021773045f7d47f6e361833d4cb5e6190bc2
Ivan <keks8953@gmail.com>
2022-11-02 23:02:36 +0200

    create main window

---
+++ Lab04_Tomchuk/Lab04_Tomchuk/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab04_Tomchuk
+{
+    public partial class Lab04_Tomchuk : Form
+    {
+        public Lab04_Tomchuk()
+        {
+            InitializeComponent();
+        }
+    }
+}
